a4ff6760585cfea714edb5fd331a57f78ba74b7c
Andrei Dobrinski <andreidobrinski@Harveys-MacBook-Air.local>
2017-09-27 19:07:06 -0400

    Create sticky navbar header

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -4,13 +4,11 @@ import './navbar.css';
 class NavBar extends Component {
   render() {
     return (
-      <nav>
-        <div className="NavBarContainer">
-          <div className="NavBar">
-            <a className="nav-text" href="#">Work</a>
-            <a className="nav-text" href="#">Play</a>
-            <a className="nav-text" href="#">Self</a>
-          </div>
+      <nav className="NavBarContainer">
+        <div className="NavBar">
+          <a className="nav-text" href="#">Work</a>
+          <a className="nav-text" href="#">Play</a>
+          <a className="nav-text" href="#">Self</a>
         </div>
       </nav>
     );
