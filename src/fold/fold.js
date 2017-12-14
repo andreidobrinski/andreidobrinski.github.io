1b54be126441eb1d880e936943d14e6d4890edc4
Andrei Dobrinski <andreidobrinski@Harveys-MacBook-Air.local>
2017-12-13 21:31:06 -0500

    refactor Fold to be SFC

--- src/fold/fold.js
+++ src/fold/fold.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from '../andreiDLogo.svg';
 import '../fold/fold.css';
 import IntroText from '../fold/introText.js';
 
-class Fold extends Component {
-	render() {
-		return (
-			<div className="fold">
-            	<img src={logo} className="logo" alt="logo" />
-            	<br />
-            	<IntroText /> 
-        	</div>
-		);
-	}
-}
+const Fold = () => {
+	return (
+		<div className="fold">
+			<img src={logo} className="logo" alt="logo" />
+			<br />
+			<IntroText />
+		</div>
+	);
+};
 
 export default Fold;
