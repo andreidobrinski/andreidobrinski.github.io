e0f72c35ac91d7c5b1fa6dfff54129c8263b199e
Andrei Dobrinski <andreidobrinski@Harveys-MacBook-Air.local>
2017-12-13 21:28:43 -0500

    rethink self

--- src/fold/introText.js
+++ src/fold/introText.js
@@ -6,7 +6,7 @@ const IntroText = (props) => {
 		<p className="intro-text">
 			Hi, I'm<br />
 			Andrei Dobrinski,<br />
-			a UX Designer with a passion for motion design.<br />
+			a UX Designer and Front-End Developer.<br />
 			Currently working at LifeTales in Toronto<br />
 			What would you like to know more about?<br />
 			My ...<br />
