82f3d41e5758778c93f1db881392d254385c5dc2
Andrei Dobrinski <andreidobrinski@Harveys-MacBook-Air.local>
2017-12-13 21:27:11 -0500

    refactor IntroText to SFC

--- src/fold/introText.js
+++ src/fold/introText.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './fold.css';
 
-class IntroText extends Component {
-	render() {
-		return (
-			<p className="intro-text">
-                Hi, I'm<br />
-                Andrei Dobrinski,<br />
-                a UX Designer with a passion for motion design.<br />
-                Currently working at LifeTales in Toronto<br />
-                What would you like to know more about?<br />
-                My ...<br />
-            </p> 
-		);
-	}
-}
+const IntroText = (props) => {
+  return (
+		<p className="intro-text">
+			Hi, I'm<br />
+			Andrei Dobrinski,<br />
+			a UX Designer with a passion for motion design.<br />
+			Currently working at LifeTales in Toronto<br />
+			What would you like to know more about?<br />
+			My ...<br />
+		</p>
+  );
+};
 
 export default IntroText;
