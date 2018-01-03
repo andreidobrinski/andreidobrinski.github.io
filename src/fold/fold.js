41cc991bfda51add872b959c576ceaf2dcfd7916
Andrei Dobrinski <andreidobrinski@Harveys-MacBook-Air.local>
2018-01-02 22:42:00 -0500

    refactor Fold to remove return statement

--- src/fold/fold.js
+++ src/fold/fold.js
@@ -3,14 +3,12 @@ import logo from '../andreiDLogo.svg';
 import '../fold/fold.css';
 import IntroText from '../fold/intro-text.js';
 
-const Fold = () => {
-	return (
-		<div className="fold">
-			<img src={logo} className="logo" alt="logo" />
-			<br />
-			<IntroText />
-		</div>
-	);
-};
+const Fold = () => (
+	<div className="fold">
+		<img src={logo} className="logo" alt="logo" />
+		<br />
+		<IntroText />
+	</div>
+);
 
 export default Fold;
