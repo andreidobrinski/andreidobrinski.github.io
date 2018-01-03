2894e814c829f43dd3481c7685d9c5f9c2a85c22
Andrei Dobrinski <andreidobrinski@Harveys-MacBook-Air.local>
2018-01-02 22:47:23 -0500

    cleanup

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LandingPage from './landing-page';
 import IntroText from '../fold/intro-text';
