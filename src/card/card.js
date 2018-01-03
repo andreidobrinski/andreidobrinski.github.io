c319930035ee2b4f4ed1ee07f509e3e10169685a
Andrei Dobrinski <andreidobrinski@Harveys-MacBook-Air.local>
2018-01-02 22:46:18 -0500

    refactor Card to be a SFC

--- src/card/card.js
+++ src/card/card.js
@@ -1,30 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './card.css';
 
-class Card extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      product: "left side",
-      skill: "right side"
-    }
-  }
-
-  render() {
-    return (
-      <div className="card">
-        <div className="overlay">
-          <div className="card-product">
-            <p>{this.state.product}</p>
-          </div>
-          <div className="card-skill">
-            <p>{this.state.skill}</p>
-          </div>
-        </div>
+const Card = (props) => (
+  <div className="card" onClick={this.handleClick}>
+    <div className="overlay">
+      <div className="card-product">
+        <p>{props.product}</p>
       </div>
-    );
-  }
-}
+      <div className="card-skill">
+        <p>{props.skill}</p>
+      </div>
+    </div>
+  </div>
+);
 
 export default Card;
