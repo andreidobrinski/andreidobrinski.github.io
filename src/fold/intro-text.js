d0d6d7b82ab7830019f802eb248537fbbab273e2
Andrei Dobrinski <andreidobrinski@Harveys-MacBook-Air.local>
2018-01-02 22:52:49 -0500

    remove return() line from IntroText

--- src/fold/intro-text.js
+++ src/fold/intro-text.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import './fold.css';
 
-const IntroText = () => {
-  return (
-		<p className="intro-text">
-			Hi, I'm<br />
-			Andrei Dobrinski,<br />
-			a UX Designer and Front-End Developer.<br />
-			Currently working at LifeTales in Toronto<br />
-			What would you like to know more about?<br />
-			My ...<br />
-		</p>
-  );
-};
+const IntroText = () => (
+	<p className="intro-text">
+		Hi, I'm<br />
+		Andrei Dobrinski,<br />
+		a UX Designer and Front-End Developer.<br />
+		Currently working at LifeTales in Toronto<br />
+		What would you like to know more about?<br />
+		My ...<br />
+	</p>
+);
 
 export default IntroText;
