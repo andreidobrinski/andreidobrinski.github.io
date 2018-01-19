96b2ccf8cec1488a1790920b849f52ac312e2270
Andrei Dobrinski <>
2018-01-18 22:06:39 -0500

    style body text

--- src/work/BadlandsBrewing/styled.js
+++ src/work/BadlandsBrewing/styled.js
@@ -11,13 +11,21 @@ export const Content = styled.div`
   grid-column-end: 3;
 `;
 
+export const Summary = styled.h2`
+  font-size: 2.619em;
 `;
 
 export const Header = styled.h3`
-
+  font-size: 2.619em;
 `;
 
 export const Body = styled.p`
+  line-height: 1.618;
+  font-size: 1.618em;
+  ${'' /* text-align: justify;
+  text-justify: inter-word; */}
+`;
+
 export const LargeImage = styled.img`
   grid-column-start: 2;
   grid-column-end: 3;
