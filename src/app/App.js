14f5b5d758e9740210b853141730e96865ab4ddf
Andrei Dobrinski <>
2018-02-04 16:30:31 -0500

    set up ThemeProvider and media queries

--- src/app/App.js
+++ src/app/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
 import LandingPage from './LandingPage';
 import BadlandsBrewing from '../work/BadlandsBrewing';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Switch>
-            <Route path="/work/BadlandsBrewing" component={BadlandsBrewing} />
-            <Route path="/" component={LandingPage} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <div>
+            <Switch>
+              <Route path="/work/BadlandsBrewing" component={BadlandsBrewing} />
+              <Route path="/" component={LandingPage} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </ThemeProvider>
     );
   }
 }
---
+++ src/app/theme.js
@@ -0,0 +1,26 @@
+import { css } from 'styled-components';
+
+const sizes = {
+  desktop: 992,
+  tablet: 768,
+}
+
+// iterate through the sizes and create a media template
+export const media = Object.keys(sizes).reduce((accumulator, label) => {
+  // use em in breakpoints to work properly cross-browser and support users
+  // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
+  const emSize = sizes[label] / 16
+  accumulator[label] = (...args) => css`
+    @media (min-width: ${emSize}em) {
+      ${css(...args)}
+    }
+  `
+  return accumulator
+}, {})
+
+export const spacer = '16px';
+
+export default {
+  media,
+  spacer,
+};
