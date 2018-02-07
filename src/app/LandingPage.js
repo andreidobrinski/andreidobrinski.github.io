a67b6156e270b0ec04b6c2b9a08692cadae201bc
Andrei Dobrinski <>
2018-02-06 19:56:55 -0500

    refactor landing page to sfc

--- src/app/LandingPage.js
+++ src/app/LandingPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
 import Fold from '../fold/Fold';
@@ -6,29 +6,25 @@ import NavBar from '../navbar/Navbar';
 import Card from '../card/Card';
 import badlandsLogo from '../work/BadlandsBrewing/assets/badlands-brewing-logo.svg';
 
-class LandingPage extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Fold />
-        <NavBar workStyle="nav-text" playStyle="nav-text-subtle" selfStyle="nav-text-subtle"/>
-        <Link to="/work/BadlandsBrewing">
-          <Card
-            product="Badlands Brewing"
-            skill="Logo Design"
-            image={badlandsLogo}
-          />
-        </Link>
-        <Card />
-        <Card />
-        <Card />
-        <NavBar workStyle="nav-text-subtle" playStyle="nav-text" selfStyle="nav-text-subtle"/>
-        <Card />
-        <Card />
-        <Card />
-      </div>
-    );
-  }
-}
+const LandingPage = () => (
+  <div className="App">
+    <Fold />
+    <NavBar workStyle="nav-text" playStyle="nav-text-subtle" selfStyle="nav-text-subtle" />
+    <Link to="/work/BadlandsBrewing">
+      <Card
+        product="Badlands Brewing"
+        skill="Logo Design"
+        image={badlandsLogo}
+      />
+    </Link>
+    <Card />
+    <Card />
+    <Card />
+    <NavBar workStyle="nav-text-subtle" playStyle="nav-text" selfStyle="nav-text-subtle" />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+);
 
 export default LandingPage;
