c34958d99ac017adc20b6c44c52be70d22aeb492
Andrei Dobrinski <>
2018-02-10 17:19:50 -0500

    linter fixes for instagram photos

---
+++ src/work/BadlandsBrewing/instagram-photos.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import InstagramEmbed from 'react-instagram-embed';
+import { InstagramWrap } from './styled';
+
+const InstagramPhotos = () => (
+  <InstagramWrap>
+    <InstagramEmbed
+      url="https://www.instagram.com/p/BedKYX5nFla/?taken-by=badlandsbrewing"
+      maxWidth={375}
+      hideCaption
+      containerTagName="div"
+      protocol=""
+      onLoading={() => {}}
+      onSuccess={() => {}}
+      onAfterRender={() => {}}
+      onFailure={() => {}}
+    />
+    <InstagramEmbed
+      url="https://www.instagram.com/p/BbCJF1xnFmV/?taken-by=badlandsbrewing"
+      maxWidth={375}
+      hideCaption
+      containerTagName="div"
+      protocol=""
+      onLoading={() => {}}
+      onSuccess={() => {}}
+      onAfterRender={() => {}}
+      onFailure={() => {}}
+    />
+  </InstagramWrap>
+);
+
+export default InstagramPhotos;
