e5a3b9b81d7fd4ee808ad1235418f285e23c11dd
Andrei Dobrinski <andreiidobrinski@gmail.com>
2018-07-12 23:04:39 -0400

    add InternalLink styles

--- src/app/styled.js
+++ src/app/styled.js
@@ -1,4 +1,5 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const App = styled.div`
   text-align: center;
@@ -7,17 +8,25 @@ export const App = styled.div`
   background-color: ${props => props.theme.colours.background};
 `;
 
-export const ExternalLink = styled.a.attrs({
-  target: '_blank',
-  rel: 'noopener noreferrer',
-})`
+const LinkStyles = css`
   color: ${props => props.theme.colours.text};
   text-decoration: none;
   padding: 0 2px;
   box-shadow: inset 0 -6px 0 0 ${props => props.theme.colours.main};
-  transition: box-shadow 0.5s;
+  transition: box-shadow 0.3s;
 
   &:hover {
     box-shadow: inset 0 -24px 0 0 ${props => props.theme.colours.main};
   }
+`;
+
+export const ExternalLink = styled.a.attrs({
+  target: '_blank',
+  rel: 'noopener noreferrer',
+})`
+  ${LinkStyles};
+`;
+
+export const InternalLink = styled(Link)`
+  ${LinkStyles};
 `;
