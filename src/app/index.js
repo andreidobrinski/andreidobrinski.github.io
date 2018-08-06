1bc8e882f77a2050c6eabb0ac43e05476f260374
Andrei Dobrinski <andreiidobrinski@gmail.com>
2018-08-06 14:47:39 -0400

    add lt to route

--- src/app/index.js
+++ src/app/index.js
@@ -5,14 +5,16 @@ import 'react-tippy/dist/tippy.css';
 import theme from './theme';
 
 import LandingPage from '../landing-page';
-import BadlandsBrewing from '../work/badlands-brewing';
+import LifeTales from '../work/lifetales';
 import AlbumPicker from '../work/album-picker';
+import BadlandsBrewing from '../work/badlands-brewing';
 import Learning from '../learning';
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <BrowserRouter>
       <Switch>
+        <Route exact path="/lifetales" component={LifeTales} />
         <Route exact path="/albumpicker" component={AlbumPicker} />
         <Route exact path="/badlands-brewing" component={BadlandsBrewing} />
         <Route exact path="/learning" component={Learning} />
