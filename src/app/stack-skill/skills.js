e5b1c7f262d5e281e5cdddbcc96cd67e71f26087
Andrei Dobrinski <andreiidobrinski@gmail.com>
2018-08-06 17:48:17 -0400

    add stack skills for web dev

--- src/app/stack-skill/skills.js
+++ src/app/stack-skill/skills.js
@@ -1,8 +1,11 @@
 import illustratorIcon from './assets/illustrator.svg';
 import reactIcon from './assets/react.svg';
+import reduxIcon from './assets/redux.svg';
 import reactRouterIcon from './assets/react-router.svg';
-import sketchIcon from './assets/sketch.svg';
+import graphqlIcon from './assets/graphql.svg';
+import gatsbyIcon from './assets/gatsby.svg';
 import styledComponentsIcon from './assets/styled-components.png';
+import sketchIcon from './assets/sketch.svg';
 import googleSheetsIcon from './assets/google-sheets.png';
 
 const Skills = {
@@ -14,17 +17,29 @@ const Skills = {
     name: 'React',
     image: reactIcon,
   },
+  redux: {
+    name: 'Redux',
+    image: reduxIcon,
+  },
   reactrouter: {
     name: 'React Router',
     image: reactRouterIcon,
   },
-  sketch: {
-    name: 'Sketch',
-    image: sketchIcon,
+  graphql: {
+    name: 'GraphQL',
+    image: graphqlIcon,
+  },
+  gatsby: {
+    name: 'Gatsby',
+    image: gatsbyIcon,
   },
   styledcomponents: {
     name: 'Styled Components',
     image: styledComponentsIcon,
+  },
+  sketch: {
+    name: 'Sketch',
+    image: sketchIcon,
   },
   googlesheets: {
     name: 'Google Sheets',
