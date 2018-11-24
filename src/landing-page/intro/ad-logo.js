c544a6230003e4364c4ab0a2040a956531e53144
Andrei Dobrinski <andreiidobrinski@gmail.com>
2018-11-24 11:25:03 -0500

    format

--- src/landing-page/intro/ad-logo.js
+++ src/landing-page/intro/ad-logo.js
@@ -4,7 +4,7 @@ import { Svg } from './styled';
 
 const AdLogo = ({ height }) => (
   <Svg width="167px" height={`${height}px`} viewBox="0 0 167 167" version="1.1" xmlns="http://www.w3.org/2000/svg">
-    <g id="logo" fillOpacity="0.8" strokeWidth="2" >
+    <g id="logo" fillOpacity="0.8" strokeWidth="2">
       <rect id="Rectangle-Copy-14" transform="translate(83.500000, 83.500000) scale(-1, -1) translate(-83.500000, -83.500000) " x="71.5" y="6.5" width="24" height="154" rx="12" />
       <rect id="Rectangle-Copy-15" transform="translate(115.911688, 115.911688) scale(-1, -1) rotate(45.000000) translate(-115.911688, -115.911688) " x="103.911688" y="57.9116882" width="24" height="116" rx="12" />
       <rect id="Rectangle-Copy-16" transform="translate(115.911688, 50.911688) scale(-1, 1) rotate(45.000000) translate(-115.911688, -50.911688) " x="103.911688" y="-7.08831175" width="24" height="116" rx="12" />
