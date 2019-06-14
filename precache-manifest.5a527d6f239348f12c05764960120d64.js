964fe6946135dee150b35e17c2bfc844ae430248
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-06-14 17:18:02 -0400

    Updates

---
+++ precache-manifest.5a527d6f239348f12c05764960120d64.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a734a3c7dabb4af16a86bb695b6c6cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0ea5eb3953e73d98cfd",
+    "url": "/static/css/2.a189aca7.chunk.css"
+  },
+  {
+    "revision": "8e95563d73a11ee62101",
+    "url": "/static/css/main.73bde696.chunk.css"
+  },
+  {
+    "revision": "e0ea5eb3953e73d98cfd",
+    "url": "/static/js/2.c0765d37.chunk.js"
+  },
+  {
+    "revision": "8e95563d73a11ee62101",
+    "url": "/static/js/main.fdc6c4a2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0cf942501f72930ba18cd306ba55d4ac",
+    "url": "/static/media/album-picker.0cf94250.svg"
+  },
+  {
+    "revision": "e2846d126ca4e122c638a7279ca65826",
+    "url": "/static/media/badlands-logo-dark.e2846d12.png"
+  },
+  {
+    "revision": "b42c430ef2ae94be82e832d7f58020ff",
+    "url": "/static/media/badlands-logo-light.b42c430e.png"
+  },
+  {
+    "revision": "aec012ae2ee1860442d37e9c7c274c57",
+    "url": "/static/media/gatsby.aec012ae.svg"
+  },
+  {
+    "revision": "c521aab8494c97c3ff7d363f7f5fe3af",
+    "url": "/static/media/google-sheets.c521aab8.png"
+  },
+  {
+    "revision": "5a4a847bcd232f4b3731bbfb9974f088",
+    "url": "/static/media/graphql.5a4a847b.svg"
+  },
+  {
+    "revision": "cee0eba49b576b96bd76c126ffac71b1",
+    "url": "/static/media/illustrator.cee0eba4.svg"
+  },
+  {
+    "revision": "fecc78123e14d99671e4102f384cdd6b",
+    "url": "/static/media/invision.fecc7812.svg"
+  },
+  {
+    "revision": "8d86cad05847fdbae962d2960bde9d7c",
+    "url": "/static/media/lifetales-logo.8d86cad0.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "85772647236e76b8e772c14c092a28ac",
+    "url": "/static/media/redux.85772647.svg"
+  },
+  {
+    "revision": "6aeae8728c6b34e6ce5f1dd6af93017b",
+    "url": "/static/media/sketch.6aeae872.svg"
+  },
+  {
+    "revision": "39353001c68abd5e4c6d3beb152a249b",
+    "url": "/static/media/spiral-one.39353001.gif"
+  },
+  {
+    "revision": "2c199c96f517487e3359c7dc5ed770e6",
+    "url": "/static/media/styled-components.2c199c96.png"
+  },
+  {
+    "revision": "0b716c2d52ec3a48fbe91ac031070c1d",
+    "url": "/static/media/swift.0b716c2d.svg"
+  },
+  {
+    "revision": "f909b843730615d7efa2d79c986152b8",
+    "url": "/static/media/xcode.f909b843.png"
+  },
+  {
+    "revision": "e52f441db61da3e51f47c45bef204007",
+    "url": "/static/media/zelda-owl.e52f441d.png"
+  }
+]);
