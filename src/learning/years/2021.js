598ba06cb91900e8c646c67fae0f9a371123cf68
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-13 20:16:16 -0500

    update learning list

--- src/learning/years/2021.js
+++ src/learning/years/2021.js
@@ -1,6 +1,20 @@
 export const year2021 = [
   { year: 2021 },
   {
+    name: 'Microfrontends with React: A Complete Developers Guide',
+    author: 'Stephen Grider',
+    type: 'Course',
+    link: 'https://www.udemy.com/course/microfrontend-course/',
+    repo: 'https://github.com/andreidobrinski/microfrontends-course-udemy',
+  },
+  {
+    name: 'Building a widget using web components',
+    author: 'Pradhumna Pancholi',
+    type: 'Workshop',
+    link: 'https://www.meetup.com/torontojs/events/ptxplrycccbqb',
+    repo: 'https://github.com/andreidobrinski/eth-widget',
+  },
+  {
     name: 'Microservices with Node JS and React',
     author: 'Stephen Grider',
     type: 'Course',
