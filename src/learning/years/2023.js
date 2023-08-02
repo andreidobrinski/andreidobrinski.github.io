b26eca44a507070ad8b934b4a518c8dad2805a74
Andrei Dobrinski <andreiidobrinski@gmail.com>
2023-08-02 13:19:03 -0400

    Update learning list with CSS course

--- src/learning/years/2023.js
+++ src/learning/years/2023.js
@@ -1,6 +1,13 @@
 export const year2023 = [
   { year: 2023 },
   {
+    name: 'CSS Animations and Transitions',
+    author: 'David Khourshid',
+    type: 'Course',
+    link: 'https://frontendmasters.com/courses/css-animations',
+    repo: 'https://github.com/andreidobrinski/css-animations-and-transitions-fm-course',
+  },
+  {
     name: 'Build a Realtime Chat App with Remix and Supabase',
     author: 'Jon Meyers',
     type: 'Course',
