6b84f4e74e60f753cd0a30f9a890f92f9cd2e4f8
andreidobrinski <andreiidobrinski@gmail.com>
2024-01-04 20:26:43 -0500

    update learning list

---
+++ src/learning/years/2024.js
@@ -0,0 +1,11 @@
+export const year2024 = [
+  { year: 2024 },
+  {
+    name: "A Tour of Web Capabilities",
+    author: "Maximiliano Firtman",
+    type: "Course",
+    link: "https://frontendmasters.com/courses/device-web-apis/",
+    repo: "https://github.com/andreidobrinski/a-tour-of-web-capabilities-fm-course",
+    inProgress: true,
+  },
+];
--- src/learning/years/index.js
+++ src/learning/years/index.js
@@ -1,12 +1,14 @@
-import { year2017 } from './2017';
-import { year2018 } from './2018';
-import { year2019 } from './2019';
-import { year2020 } from './2020';
-import { year2021 } from './2021';
-import { year2022 } from './2022';
-import { year2023 } from './2023';
+import { year2017 } from "./2017";
+import { year2018 } from "./2018";
+import { year2019 } from "./2019";
+import { year2020 } from "./2020";
+import { year2021 } from "./2021";
+import { year2022 } from "./2022";
+import { year2023 } from "./2023";
+import { year2024 } from "./2024";
 
 export const learning = [
+  ...year2024,
   ...year2023,
   ...year2022,
   ...year2021,
