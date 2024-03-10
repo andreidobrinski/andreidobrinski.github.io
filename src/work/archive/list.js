a6a458ea83ce110471df8ca3ca450177b19220fc
andreidobrinski <andreiidobrinski@gmail.com>
2024-03-10 14:12:50 -0400

    run prettier on project archive list

--- src/work/archive/list.js
+++ src/work/archive/list.js
@@ -1,38 +1,40 @@
 const ArchiveList = [
   {
-    name: 'Gen Art Canvas',
-    description: 'A UI for playing around with parameters in a generative art example',
-    tech: ['React', 'Canvas'],
-    demo: 'https://andreidobrinski.com/gen-art-canvas/',
-    repo: 'https://github.com/andreidobrinski/gen-art-canvas',
+    name: "Gen Art Canvas",
+    description:
+      "A UI for playing around with parameters in a generative art example",
+    tech: ["React", "Canvas"],
+    demo: "https://andreidobrinski.com/gen-art-canvas/",
+    repo: "https://github.com/andreidobrinski/gen-art-canvas",
   },
   {
-    name: 'Produce in Season',
-    description: 'An app to show what produce is in season this month in Ontario',
-    tech: ['Svelte'],
-    demo: 'https://andreidobrinski.com/produce-in-season/',
-    repo: 'https://github.com/andreidobrinski/produce-in-season',
+    name: "Produce in Season",
+    description:
+      "An app to show what produce is in season this month in Ontario",
+    tech: ["Svelte"],
+    demo: "https://andreidobrinski.com/produce-in-season/",
+    repo: "https://github.com/andreidobrinski/produce-in-season",
   },
   {
-    name: 'Recipes',
-    description: 'A cookbook filled with recipes and checklists',
-    tech: ['React', 'Gatsby', 'MDX'],
-    demo: 'https://andreidobrinski.com/recipes/',
-    repo: 'https://github.com/andreidobrinski/recipes',
+    name: "Recipes",
+    description: "A cookbook filled with recipes and checklists",
+    tech: ["React", "Gatsby", "MDX"],
+    demo: "https://andreidobrinski.com/recipes/",
+    repo: "https://github.com/andreidobrinski/recipes",
   },
   {
-    name: 'RL Random Camera Settings Generator',
-    description: 'Generate random camera settings in Rocket League',
-    tech: ['React', 'Gatsby'],
-    demo: 'https://andreidobrinski.com/rl-random-camera-settings/',
-    repo: 'https://github.com/andreidobrinski/rl-random-camera-settings',
+    name: "RL Random Camera Settings Generator",
+    description: "Generate random camera settings in Rocket League",
+    tech: ["React", "Gatsby"],
+    demo: "https://andreidobrinski.com/rl-random-camera-settings/",
+    repo: "https://github.com/andreidobrinski/rl-random-camera-settings",
   },
   {
-    name: 'LoadingAnimation',
-    description: 'A loading animation in Swift',
-    tech: ['Swift', 'Animation'],
-    repo: 'https://github.com/andreidobrinski/LoadingAnimations',
-  }
+    name: "LoadingAnimation",
+    description: "A loading animation in Swift",
+    tech: ["Swift", "Animation"],
+    repo: "https://github.com/andreidobrinski/LoadingAnimations",
+  },
 ];
 
 export default ArchiveList;
