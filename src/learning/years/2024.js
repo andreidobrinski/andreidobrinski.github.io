a45cb51cf69748d3448b99e5376dae4f0fdd0952
Andrei Dobrinski <andreiidobrinski@gmail.com>
2024-06-21 14:17:25 -0400

    Update learning list

--- src/learning/years/2024.js
+++ src/learning/years/2024.js
@@ -1,6 +1,13 @@
 export const year2024 = [
   { year: 2024 },
   {
+    name: "Guide for Launching Your Next Big Idea",
+    author: "Paul Boag",
+    type: "Course",
+    link: "https://frontendmasters.com/courses/product-launch/",
+    repo: "https://github.com/andreidobrinski/guide-for-launching-your-next-big-idea-fm-course",
+  },
+  {
     name: "A Tour of Web Capabilities",
     author: "Maximiliano Firtman",
     type: "Course",
