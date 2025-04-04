4c3fe6dc070d236b54b6d066ef070fa74b10f5f2
andreidobrinski <andreiidobrinski@gmail.com>
2025-04-04 12:21:04 -0400

    add workshop to learning list

--- src/learning/years/2025.js
+++ src/learning/years/2025.js
@@ -1,6 +1,13 @@
 export const year2025 = [
   { year: 2025 },
   {
+    name: "Bringing Animations to Life: Interactive dotLottie with JavaScript 🚀",
+    author: "Karin Fam",
+    type: "Workshop",
+    link: "https://guild.host/events/bringing-animations-b37mq0",
+    repo: "https://github.com/andreidobrinski/toronto-js-workshop-lottie-25-05-2025",
+  },
+  {
     name: "Data Governance Workshop (GDPR)",
     author: "InfoTrust",
     type: "Workshop",
